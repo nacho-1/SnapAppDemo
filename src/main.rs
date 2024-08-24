d67da8fd1ee064bbba8518a89092fcc0402b6782
ibertolini <56204692+nacho-1@users.noreply.github.com>
2024-08-24 17:10:48 -0300

    feat: Agrego los endpoints principales
    GET y POST a /snaps
    Modifico el puerto default de 3000 a 8080

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::env;
+use axum::routing::get;
 
 #[tokio::main]
 pub async fn main() {
     let app = axum::Router::new()
         .fallback(
             fallback_handler
+        )
+        .route(
+            "/snaps",
+            get(snaps_get_handler)
+                .post(snaps_post_handler)
         );
 
-    let mut addr = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let mut addr = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
     addr.insert_str(0, "0.0.0.0:");
     let listener = tokio::net::TcpListener::bind(addr)
         .await
@@ -22,4 +28,14 @@ async fn fallback_handler(
     uri: axum::http::Uri
 ) -> impl axum::response::IntoResponse {
     (axum::http::StatusCode::NOT_FOUND, format!("No route {}", uri))
+}
+
+async fn snaps_get_handler()
+-> (axum::http::StatusCode, String) {
+    (axum::http::StatusCode::OK, "A list of snaps\n".to_string())
+}
+
+async fn snaps_post_handler()
+-> (axum::http::StatusCode, String) {
+    (axum::http::StatusCode::CREATED, "Snap created successfully\n".to_string())
 }
