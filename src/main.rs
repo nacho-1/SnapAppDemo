f677306346bcfb8f9dbefc532b6581cd3a2d8642
ibertolini <56204692+nacho-1@users.noreply.github.com>
2024-08-24 16:32:42 -0300

    config: Inicializo frameworks
    Seteo axum y tokio.
    Implemento app basica.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+pub async fn main() {
+    let app = axum::Router::new()
+        .fallback(
+            fallback_handler
+        );
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+/// axum handler for any request that fails to match the router routes.
+/// This implementation returns HTTP status code Not Found (404).
+async fn fallback_handler(
+    uri: axum::http::Uri
+) -> impl axum::response::IntoResponse {
+    (axum::http::StatusCode::NOT_FOUND, format!("No route {}", uri))
 }
