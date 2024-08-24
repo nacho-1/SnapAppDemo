b11cc8112d458782c76e6476e400998d82d599ad
ibertolini <56204692+nacho-1@users.noreply.github.com>
2024-08-24 16:52:48 -0300

    feat: Agrego env var para el puerto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 #[tokio::main]
 pub async fn main() {
     let app = axum::Router::new()
@@ -5,7 +7,9 @@ pub async fn main() {
             fallback_handler
         );
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let mut addr = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    addr.insert_str(0, "0.0.0.0:");
+    let listener = tokio::net::TcpListener::bind(addr)
         .await
         .unwrap();
 
