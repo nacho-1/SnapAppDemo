5eab33bf5660bf65787a9001a59c6e7cf9c29d64
ibertolini <56204692+nacho-1@users.noreply.github.com>
2024-08-30 16:57:54 -0300

    tests: Agrego test de integracion

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -158,3 +158,42 @@ async fn get_snaps() {
     assert_eq!(body["data"][0]["message"], json!("Test Snap 2"));
     assert_eq!(body["data"][1]["message"], json!("Test Snap 1"));
 }
+
+#[tokio::test]
+async fn get_zero_snaps() {
+    let state = state::MockSnapRepository::new();
+    let app = router::get_router().with_state(state);
+
+    let listener = TcpListener::bind("localhost:0")
+        .await
+        .unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    // Run the server in a thread.
+    tokio::spawn(async move {
+        axum::serve(listener, app).await.unwrap();
+    });
+
+    // Create a client to communicate with the server.
+    let client =
+        hyper_util::client::legacy::Builder::new(hyper_util::rt::TokioExecutor::new())
+            .build_http();
+
+    let response = client
+        .request(
+            Request::builder()
+                .method("GET")
+                .uri(format!("http://{addr}/snaps"))
+                .body(Body::empty())
+                .unwrap(),
+        ).await.unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = response.into_body()
+        .collect()
+        .await
+        .unwrap()
+        .to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    assert_eq!(body["data"].as_array().unwrap().len(), 0);
+}
