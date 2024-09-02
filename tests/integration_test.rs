5c145a426b273674a92203a727e4996b01513b25
ibertolini <56204692+nacho-1@users.noreply.github.com>
2024-09-02 19:00:56 -0300

    tests: Agrego test de integracion

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -365,3 +365,53 @@ async fn post_invalid_request_missing_header() {
     let body: Value = serde_json::from_slice(&body).unwrap();
     assert_eq!(body["title"], json!("Problem Parsing Json"));
 }
+
+#[tokio::test]
+async fn post_snap_extended_json() {
+    // What happens if json has more values than used?
+    let state = state::MockSnapRepository::new();
+    let app = router::get_router().with_state(state);
+
+    let listener = TcpListener::bind("localhost:0")
+        .await
+        .unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    // Run the server in a thread.
+    tokio::spawn(async move {
+        axum::serve(listener, app).await.unwrap();
+    });
+
+    // Create a client to communicate with the server.
+    let client =
+        hyper_util::client::legacy::Builder::new(hyper_util::rt::TokioExecutor::new())
+            .build_http();
+
+    let message = "Test123";
+    let response = client
+        .request(
+            Request::builder()
+                .method("POST")
+                .uri(format!("http://{addr}/snaps"))
+                .header("Content-Type", "application/json")
+                .body(Body::from(
+                    serde_json::to_string(&json!({
+                        "first_field": "Value",
+                        "message": message,
+                        "other_field": "Value",
+                    })).unwrap()
+                ))
+                .unwrap(),
+        ).await.unwrap();
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+    let body = response.into_body()
+        .collect()
+        .await
+        .unwrap()
+        .to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    assert_eq!(body["data"]["message"], json!(message));
+    let id = body["data"]["id"].as_str().unwrap();
+    assert!(Uuid::parse_str(id).is_ok());
+}
